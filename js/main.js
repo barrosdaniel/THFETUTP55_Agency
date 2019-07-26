e8c4820424fa740f4613d90e9c324ed879b8531c
barros.daniel@outlook.com <barros.daniel@outlook.com>
2019-07-26 20:09:19 +0800

    feature: Add alternative text to navigation bar when sticky

--- js/main.js
+++ js/main.js
@@ -10,4 +10,12 @@ $(document).ready(function () {
   $(".header").sticky({
     topSpacing: 0
   });
+
+  $('.header').on('sticky-start', function () {
+    $('.description').html('We build <strong>great</strong> apps');
+  });
+
+  $('.header').on('sticky-end', function () {
+    $('.description').html('We build apps');
+  });
 });
