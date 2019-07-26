6258e14f166a92547058721df9b2fa0d03224c9c
barros.daniel@outlook.com <barros.daniel@outlook.com>
2019-07-26 21:15:45 +0800

    feature: Add sticky 'Want us to work on your project?' bar

--- js/main.js
+++ js/main.js
@@ -1,3 +1,5 @@
+// Animsition jQuery Plugin
+// Fade in and out page transitions
 $(".animsition").animsition({
   inClass: 'fade-in',
   outClass: 'fade-out',
@@ -6,6 +8,8 @@ $(".animsition").animsition({
   outDuration: 500
 });
 
+// Sticky jQuery Plugin
+// Make navigation bar sticky
 $(document).ready(function () {
   $(".header").sticky({
     topSpacing: 0
@@ -19,3 +23,21 @@ $(document).ready(function () {
     $('.description').html('We build apps');
   });
 });
+
+// work.html > Make 'Want us to work on your project?' sticky and add email link
+$(document).ready(function () {
+  $('.grid-full h5').sticky({
+    topSpacing: 64
+  })
+
+  const emailLink = ` <a href="mailto: barros.daniel@outlook.com">Email</a>`;
+
+  $('.grid-full h5').on('sticky-start', function () {
+    $(this).append(emailLink);
+  });
+
+  $('.grid-full h5').on('sticky-end', function () {
+    $(this).html('Want us to work on your project?');
+  });
+
+})
