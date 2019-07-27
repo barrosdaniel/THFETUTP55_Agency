783f3d99d785ed7827d0fc46cbe461c73877dcec
barros.daniel@outlook.com <barros.daniel@outlook.com>
2019-07-28 07:40:01 +0800

    chore: Add new starter files in (Slick)

--- js/main.js
+++ js/main.js
@@ -1,49 +1,57 @@
-// Animsition jQuery Plugin
-// Fade in and out page transitions
 $(".animsition").animsition({
-  inClass: 'fade-in',
-  outClass: 'fade-out',
+  inClass: 'fade-in-right-lg',
+  outClass: 'fade-out-right-lg',
   linkElement: 'header a',
   inDuration: 1000,
   outDuration: 500
 });
 
-$('header').sticky({
+$('.header').sticky({
   getWidthFrom: '.container',
   responsiveWidth: true
-})
-
-// Sticky jQuery Plugin
-// Make navigation bar sticky
-$(document).ready(function () {
-  $(".header").sticky({
-    topSpacing: 0
-  });
-
-  $('.header').on('sticky-start', function () {
-    $('.description').html('We build <strong>great</strong> apps');
-  });
-
-  $('.header').on('sticky-end', function () {
-    $('.description').html('We build apps');
-  });
 });
 
-// work.html > Make 'Want us to work on your project?' sticky and add email link
-$(document).ready(function () {
-  $('.grid-full h5').sticky({
-    topSpacing: 64,
-    getWidthFrom: '.container',
-    responsiveWidth: true
-  })
+$('.header').on('sticky-start', function () {
+  $('.description').html('We build <strong>great</strong> apps');
+});
 
-  $('.grid-full h5').on('sticky-start', function () {
-    const emailLink = ` <a href="mailto: barros.daniel@outlook.com">Email</a>`;
-    $(this).append(emailLink);
-  });
+$('.header').on('sticky-end', function () {
+  $('.description').html('We build apps');
+});
 
-  $('.grid-full h5').on('sticky-end', function () {
-    $(this).html('Want us to work on your project?');
-  });
+$('.work').sticky({
+  topSpacing: 60,
+  getWidthFrom: '.container',
+  responsiveWidth: true
+});
+$('.work').on('sticky-start', function() {
+  $(this).append(' <a href="mailto:email@website.com" class="email-text">Email&nbsp;us</a>');
+});
+$('.work').on('sticky-end', function() {
+    $('.email-text').remove();
+});
 
-})
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
